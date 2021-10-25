f1df80c7db98dc864873d793e8529f51e350d946
senyaa1 <zloykoteyka145@gmail.com>
2021-10-25 16:02:34 +0300

    Compile fix

--- config.def.h
+++ config.def.h
@@ -70,7 +70,9 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask
+#define ALTMODE Mod1Mask
+
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
